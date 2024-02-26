5ce4a6b699cfccc66a96e19fc3adfb78e4adf91d
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2024-02-26 21:18:12 +0900

    [level 1] Title: 덧칠하기, Time: 1.04 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        
+        int answer = 1;
+        int start = section[0];
+        int end = section[0] + (m - 1);
+        
+        for (int i : section) {
+            if (i >= start && i <= end) {
+                continue;
+            }
+            else {
+                start = i;
+                end = i + (m - 1);
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
