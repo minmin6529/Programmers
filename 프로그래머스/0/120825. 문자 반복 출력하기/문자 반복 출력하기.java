2499c233b03e56b544f16359ee20b5466e531c8d
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2024-02-28 00:06:56 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 1.58 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        for (String s : my_string.split("")) {
+            for (int i = 0; i < n; i++) {
+                answer += s;
+            }
+        }
+        return answer;
+    }
+}
