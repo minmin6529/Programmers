bf0244d0ddb9ad47cfeaa0c0dea699fc4dca928e
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2024-03-13 20:50:08 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        for (int i = 0; i < my_string.length(); i++) {
+            char ch = my_string.charAt(i);
+            if (Character.isDigit(ch)) {
+                answer += Character.getNumericValue(ch);
+            }
+        }
+        return answer;
+    }
+}
