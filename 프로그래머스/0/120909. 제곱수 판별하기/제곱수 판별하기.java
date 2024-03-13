cd9744dea5ede33a3db1740a255ecfe8f4081abc
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2024-03-13 20:53:08 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 2;
+        for (int i = 0; i < n; i++) {
+            if (i * i == n) {
+                answer = 1;
+            }
+        }
+        return answer;
+    }
+}
