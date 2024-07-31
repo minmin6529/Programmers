2ec27f187f5402bd8db3226a82f0a033eb982523
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2024-07-31 22:29:55 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+        
+        if (ineq.equals("<") && eq.equals("=")) {
+            if (n <= m) {
+                answer = 1;
+            } else {
+                answer = 0;
+            }
+        }
+        
+        if (ineq.equals("<") && eq.equals("!")) {
+            if (n < m) {
+                answer = 1;
+            } else {
+                answer = 0;
+            }
+        }
+        
+        if (ineq.equals(">") && eq.equals("=")) {
+            if (n >= m) {
+                answer = 1;
+            } else {
+                answer = 0;
+            }
+        }
+        
+        if (ineq.equals(">") && eq.equals("!")) {
+            if (n > m) {
+                answer = 1;
+            } else {
+                answer = 0;
+            }
+        }
+        
+        return answer;
+    }
+}
