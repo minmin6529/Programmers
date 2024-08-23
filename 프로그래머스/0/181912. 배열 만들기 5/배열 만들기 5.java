0e3ec057e0490fc39f84c59a1e72fdad119692ac
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2024-08-23 23:47:17 +0900

    [level 0] Title: 배열 만들기 5, Time: 5.77 ms, Memory: 98.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < intStrs.length; i++) {
+            int str = Integer.parseInt(intStrs[i].substring(s, s + l));
+            if (str > k) {
+                list.add(str);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
