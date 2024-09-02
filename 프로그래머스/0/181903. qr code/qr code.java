0f4a4974e535140a839519665eb98f3cad35dc36
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2024-09-02 23:21:55 +0900

    [level 0] Title: qr code, Time: 15.88 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int q, int r, String code) {
+        String answer = "";
+        
+        for (int i = 0; i < code.length(); i++) {
+            if (i % q == r) {
+                answer += code.charAt(i);
+            }
+        }
+        
+        return answer;
+    }
+}
