2b2da98a175f70302215fc4d8e35626857f94642
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2024-09-22 23:45:21 +0900

    [level 0] Title: A로 B 만들기, Time: 1.06 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String before, String after) {
+        int answer = -1;
+        char[] bef = before.toCharArray();
+        char[] aft = after.toCharArray();
+        Arrays.sort(bef);                 
+        Arrays.sort(aft);                   
+        
+        if (Arrays.equals(bef, aft)) { 
+                answer = 1;                 
+            } else {                      
+                answer = 0;               
+            }
+        return answer;
+    }
+}
