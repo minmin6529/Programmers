02ec4481aa163741673df40a7bdc776ea6da7438
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2024-10-21 23:54:26 +0900

    [level 1] Title: 폰켓몬, Time: 4.70 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+
+        int max = nums.length / 2;
+        HashSet<Integer> numSet = new HashSet<>();
+        
+        for (int num : nums) {
+            numSet.add(num);
+        }
+        
+        if (numSet.size() > max) {
+            return max;
+        } else {
+            return numSet.size();
+        }
+    }
+}
