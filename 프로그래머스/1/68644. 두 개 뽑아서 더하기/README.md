0be13238f5d5174f8faaf034b9cdf26810196a36
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2025-03-12 09:32:32 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 8.05 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
 
 ### 성능 요약
 
-메모리: 77.7 MB, 시간: 0.72 ms
+메모리: 72.1 MB, 시간: 8.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 1일 2:6:33
+2025년 03월 12일 09:32:28
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,17 +1,14 @@
 import java.util.*;
 
 class Solution {
-    public Integer[] solution(int[] numbers) {
-        
-        Set<Integer> set = new HashSet<Integer>();
-        for (int i = 0; i < numbers.length; i++) {
+    
+    public int[] solution(int[] numbers) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length - 1; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
                 set.add(numbers[i] + numbers[j]);
             }
         }
-        
-        Integer[] answer = set.toArray(new Integer[0]);
-        Arrays.sort(answer);
-        return answer;
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 }
