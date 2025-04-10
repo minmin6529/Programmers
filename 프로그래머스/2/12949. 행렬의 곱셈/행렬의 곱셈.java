54e2794ba7e5ea8856e5e315ca1d9856e43b9db9
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2025-04-11 02:37:12 +0900

    [level 2] Title: 행렬의 곱셈, Time: 10.74 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -8,7 +8,6 @@ class Solution {
         int c2 = arr2[0].length;
         
         int[][] answer = new int[r1][c2];
-        
         for (int i = 0; i < r1; i++) {
             for (int j = 0; j < c2; j++) {
                 for (int k = 0; k < c1; k++) {
