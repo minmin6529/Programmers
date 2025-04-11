6c954a420ccb321a85a2e6c0f452df46c882e92a
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2025-04-11 15:21:36 +0900

    [level 1] Title: 실패율, Time: 23.67 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 class Solution {
-    
     public int[] solution(int N, int[] stages) {
         int[] challenger = new int[N + 2];
+        
         for (int i = 0; i < stages.length; i++) {
             challenger[stages[i]] += 1;
         }
@@ -14,7 +14,7 @@ class Solution {
         for (int i = 1; i <= N; i++) {
             if (challenger[i] == 0) {
                 fails.put(i, 0.);
-            } 
+            }
             else {
                 fails.put(i, challenger[i] / total);
                 total -= challenger[i];
