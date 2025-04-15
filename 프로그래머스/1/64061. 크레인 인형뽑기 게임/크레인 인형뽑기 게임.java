3c928e0ebbed2248b102b3b3ab05ed2fff97ee9f
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2025-04-15 22:25:17 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.32 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        
+        Stack<Integer>[] lanes = new Stack[board.length];
+        for (int i = 0; i < lanes.length; i++) {
+            lanes[i] = new Stack<>();
+        }
+        
+        for (int i = board.length - 1; i >= 0; i--) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] > 0) {
+                    lanes[j].push(board[i][j]);
+                }
+            }
+        }
+        
+        Stack<Integer> bucket = new Stack<>();
+        
+        int answer = 0;
+        
+        for (int move : moves) {
+            if (!lanes[move - 1].isEmpty()) {
+                int doll = lanes[move - 1].pop();
+                
+                if (!bucket.isEmpty() && bucket.peek() == doll) {
+                    bucket.pop();
+                    answer += 2;
+                }
+                else {
+                    bucket.push(doll);
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
