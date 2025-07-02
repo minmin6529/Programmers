6f0048ea03ad03f4f029ce1e2a027768f9faa723
Cho MinJi <105896114+minmin6529@users.noreply.github.com>
2025-07-02 17:29:07 +0900

    [level 1] Title: 모의고사, Time: 5.57 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,45 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        
         int[][] pattern = {
             {1, 2, 3, 4, 5},
             {2, 1, 2, 3, 2, 4, 2, 5},
             {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
         };
         
-        int[] score = new int[3];
+        int[] scores = new int[3];
         
         for (int i = 0; i < answers.length; i++) {
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < pattern.length; j++) {
                 if (answers[i] == pattern[j][i % pattern[j].length]) {
-                    score[j] += 1;
+                    scores[j]++;
                 }
             }
         }
         
-        int max = 0;
-        for (int i = 0; i < 3; i++) {
-            if (score[i] > max) {
-                max = score[i];
+        int maxScore = Arrays.stream(scores).max().getAsInt();
+        
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == maxScore) {
+                answer.add(i + 1);
             }
         }
         
-        int cnt = 0;
-        for (int i = 0; i < 3; i++) {
-            if (score[i] == max) {
-                cnt++;
-            }
-        }
-        
-        int[] answer = new int[cnt];
-        int num = 0;
-        for (int i = 0; i < 3; i++) {
-            if (score[i] == max) {
-                answer[num++] = i + 1;
-            }
-        }
-        
-        return answer;
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
